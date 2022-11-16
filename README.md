9bdb2ed0e617eb2bfdf5aa631aa85fd161ce4b4e
DylanCas <djcasabona12@gmail.com>
2022-11-16 13:40:00 -0700

    added api key and fetch request to get data, will add state requirement later. Added some skeleton html for search button and text input

--- README.md
+++ README.md
@@ -40,3 +40,6 @@ THEN I am presented with a 5-day forecast that displays the date, an icon repres
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
 ```
+
+## Mock Up
+![screenshot](./assets/images/06-server-side-apis-homework-demo.png)
---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=f95c2d04f516fc0e509fd5fd102333d4
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=f95c2d04f516fc0e509fd5fd102333d4
+var searchCity = document.getElementById("searchCity")
+
+searchCity.addEventListener("click", function() {
+    var cityName = document.getElementById("citySearch").value
+    console.log(cityName)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=f95c2d04f516fc0e509fd5fd102333d4`)
+.then(function (response){
+    console.log(response)
+    return response.json();
+}).then(function(data){
+    console.log(data)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=f95c2d04f516fc0e509fd5fd102333d4`)
+    .then(function (response){
+        return response.json();
+    }).then(function(data){
+        console.log(data)
+        // create elements, append elements with data indexes
+        
+    })
+})
+})
+
+
